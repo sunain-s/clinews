b09715af4ff3cac28aaa2996a937b3f6f569370b
Sunain <sas@drsyed.org>
2024-03-19 17:35:38 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ use dotenv::dotenv;
 use colour::{yellow, blue};
 use newsapi::{Articles, get_articles};
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+fn render_articles(articles: &Articles, n: usize) {
+    for i in 1..n + 1 {
+        yellow!("> {}\n", &articles.articles[i].title);
+        blue!("> {}\n\n", &articles.articles[i].url);
+    }
+}
 
 fn readline_string_clean() -> String {
     let mut s: String = String::new();
@@ -20,45 +27,22 @@ fn readline_string_clean() -> String {
     return s;
 }   
 
-fn render_articles(articles: &Articles) {
-    for a in &articles.articles {
-        yellow!("> {}\n", a.title);
-        blue!("> {}\n\n", a.url);
-    }
-}
-
-fn top_headlines() -> String {
-    print!("Enter 2-letter ISO 3166-1 country code:  ");
-    let country_code: String = readline_string_clean();
-    let url: String = format!("https://newsapi.org/v2/top-headlines?country={}&apiKey=", country_code);
-    return url;
-}
-
-fn search_headlines() -> String {
-    print!("Enter 2-letter ISO-639-1 language code:  ");
-    let lang_code: String = readline_string_clean();
-
-    print!("Enter search term:  ");
-    let search_term: String = readline_string_clean();
-
-    print!("Enter number of results:  ");
-    let num_results: usize = readline_string_clean().parse().unwrap();
-
-    let url: String = format!("https://newsapi.org/v2/everything?q={}&searchin=title,description&language={}&pageSize={}&apiKey=", search_term, lang_code, num_results);
-    return url;
-}
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------
-
 fn main() -> Result<(), Box<dyn Error>>{
     dotenv()?;
     let api_key: String = std::env::var("API_KEY")?;
-    
-    println!("Enter corresponding number to choose:\n\n1) View top headlines\n2) Search for results");
 
-    
-    //let articles: Articles = get_articles(&url)?;
-    // render_articles(&articles);
+    println!("Enter language code:");
+    let lang_code: String = readline_string_clean();
+    println!("Enter country code:");
+    let _country_code: String = readline_string_clean();
+    println!("Enter search term:");
+    let search_term: String = readline_string_clean();
+    println!("Enter number of results:");
+    let num_results: usize = readline_string_clean().parse().unwrap();
+    println!("{num_results}");
+
+    let url: String = format!("https://newsapi.org/v2/everything?q={}&searchin=title,description&language={}&apiKey={}", search_term, lang_code, api_key);
+    let articles: Articles = get_articles(&url)?;
+    render_articles(&articles, num_results);
     Ok(())
 }
-  
